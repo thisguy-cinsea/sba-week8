81bb84b244bd9e429ad42b2227babf268646ae1f
leon hunter <xleonhunter@gmail.com>
2020-02-12 21:17:02 -0500

    added comments

--- src/test/java/com/github/perscholas/service/courseservice/GetAllCoursesTest.java
+++ src/test/java/com/github/perscholas/service/courseservice/GetAllCoursesTest.java
@@ -1,8 +1,41 @@
 package com.github.perscholas.service.courseservice;
 
+import com.github.perscholas.JdbcConfigurator;
+import com.github.perscholas.utils.DirectoryReference;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * @author leonhunter
  * @created 02/12/2020 - 8:26 PM
  */
 public class GetAllCoursesTest {
+    @Before // TODO - Use files to execute SQL commands
+    public void setup() {
+        DirectoryReference directoryReference = DirectoryReference.RESOURCE_DIRECTORY;
+        File coursesSchemaFile = directoryReference.getFileFromDirectory("courses.create-table.sql");
+        File studentsSchemaFile = directoryReference.getFileFromDirectory("students.create-table.sql");
+        File coursesPopulatorFile = directoryReference.getFileFromDirectory("courses.populate-table.sql");
+        File studentsPopulatorFile = directoryReference.getFileFromDirectory("students.populate-table.sql");
+        File[] filesToExecute = new File[]{
+                coursesSchemaFile,
+                studentsSchemaFile,
+                coursesPopulatorFile,
+                studentsPopulatorFile
+        };
+    }
+
+    // given
+    private void test() {
+        JdbcConfigurator.initialize();
+
+        // when
+        // TODO - define `when` clause
+
+
+        // then
+        // TODO - define `then` clause
+    }
 }
