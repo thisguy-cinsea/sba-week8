9250ded42d8e95668537a10120cf91da10068f51
leon hunter <xleonhunter@gmail.com>
2020-02-12 20:30:42 -0500

    updated infrastructure

--- src/main/java/com/github/perscholas/JdbcConfigurator.java
+++ src/main/java/com/github/perscholas/JdbcConfigurator.java
@@ -1,8 +1,18 @@
 package com.github.perscholas;
 
+import java.sql.Connection;
+
 /**
  * @author leonhunter
  * @created 02/12/2020 - 8:27 PM
  */
 public class JdbcConfigurator {
+    // TODO - Implement singleton design to reference a single connection-instance across classes
+    public static synchronized Connection getConnection() {
+        return null;
+    }
+
+    public static void initialize() {
+        // TODO - Implement JDBC registration process
+    }
 }
--- src/main/java/com/github/perscholas/MainApplication.java
+++ src/main/java/com/github/perscholas/MainApplication.java
@@ -2,6 +2,7 @@ package com.github.perscholas;
 
 public class MainApplication {
     public static void main(String[] args) {
+        JdbcConfigurator.initialize();
         Runnable sms = new SchoolManagementSystem();
         sms.run();
     }
